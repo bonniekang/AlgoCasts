8b21aafacbf1069f6cffe3293e67cf44adc364cb
jihye kang <jihye59kang@gmail.com>
2021-08-02 23:36:39 +0100

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    for (let word of str.toLowerCase()){
+        if(vowels.includes(word)){
+            count ++
+        }
+    }
+    return count
+}
 
 module.exports = vowels;
