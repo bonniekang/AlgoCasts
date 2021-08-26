015dd632890ab1252cddd78aab1c015463a7c27f
jihye kang <jihye59kang@gmail.com>
2021-08-27 00:04:52 +0100

    foreach fn

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -156,6 +156,16 @@ class LinkedList {
 
     }
 
+    forEach(fn){
+        let node = this.head;
+        let counter = 0;
+        while(node){
+            fn(node, counter)
+            node = node.next
+            counter++
+        }
+    }
+
     *[Symbol.iterator](){
         let node = this.head;
         while(node){
