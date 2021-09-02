7adad97e32c41518c9dd967b9667a5948f2639c7
jihye kang <jihye59kang@gmail.com>
2021-09-02 22:15:40 +0100

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const array = [root, 's']
+    const result = [0]
+
+    while(array.length > 1){
+        const node = array.shift()
+        
+        if(node === 's'){
+            result.push(0)
+            array.push('s')
+        } else{
+            array.push(...node.children)
+            result[result.length - 1] ++
+        }
+    }
+
+    return result
+}
 
 module.exports = levelWidth;
